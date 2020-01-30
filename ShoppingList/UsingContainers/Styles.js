9df392bb043798c5bc48b22081518a4f5181a9c7
Jin An <jinsungan@gmail.com>
2020-01-30 11:53:33 -0500

    Create Styles.js

---
+++ ShoppingList/UsingContainers/Styles.js
@@ -0,0 +1,30 @@
+import {
+    StyleSheet
+} from "react-native"
+
+export default StyleSheet.create({
+    view: {
+        marginTop: 30,
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: '#FEFFD3',
+        flex: 1
+    },
+    view1: {
+        backgroundColor: '#68CA5B',
+        flex: 1,
+        flexDirection: "row"
+    },
+    view2: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    shopListImages: {
+        flex: 1
+    },
+    shopCaption: {
+        flex: 1,
+        justifyContent: "center",
+        alignSelf: "center"
+    }
+});
