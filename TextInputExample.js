849a84e2222881e9b44f8c22f544f17dceaa4e61
Jin An <jinsungan@gmail.com>
2020-02-13 10:19:47 -0500

    Update TextInputExample.js

--- TextInputExample.js
+++ TextInputExample.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import { Text, View, TextInput, Button, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 export default class Blink extends Component {
 
   //state object
-   constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       name: '',
@@ -21,25 +21,44 @@ export default class Blink extends Component {
 
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(name) => this.setState({name})}
-          value={this.state.name}
-        />
-        <TextInput
-          style={{height: 40}}
-          placeholder="Some more text!"
-          onChangeText={(major) => this.setState({major})}
-          value={this.state.major}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.name + "\n" + this.state.major}
+      <View style={styles.mainView}>
+        <KeyboardAvoidingView behavior="padding" enabled keyboardVerticalOffset='200'>
+          <TextInput
+            style={styles.formFields}
+            placeholder="Name"
+            placeholderTextColor="black"
+            onChangeText={(name) => this.setState({ name })}
+            value={this.state.name}
+          />
+          <TextInput
+            style={styles.formFields}
+            placeholder="Major"
+            placeholderTextColor="black"
+            onChangeText={(major) => this.setState({ major })}
+            value={this.state.major}
+          />
+          <Text style={{ padding: 10, fontSize: 42 }}>
+            {this.state.name + "\n" + this.state.major}
 
-        </Text>
-        <Button title="clear" onPress={this.clearHandler.bind(this)} />
+          </Text>
+          <Button title="clear" onPress={this.clearHandler.bind(this)} />
+        </KeyboardAvoidingView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flexDirection: 'column',
+    marginTop: 30,
+    backgroundColor: '#C3FFBA'
+  },
+  formFields: {
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    padding: 5,
+    borderWidth: 1
+  }
+})
